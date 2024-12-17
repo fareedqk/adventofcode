02c3b4daf36156f69f5ec4c5ea3df30d43df1485
fareed23 <faroodcp9@gmail.com>
2024-12-17 14:12:31 +0500

    day 17 completed

---
+++ 2024/day17/2.py
@@ -0,0 +1,60 @@
+import re
+from collections import *
+import numpy as np
+
+a = 66752888  
+b = 0         
+c = 0    
+prog = [2,4,1,7,7,5,1,7,0,3,4,1,5,5,3,0]
+
+def run(prog, a, b, c):
+    def combo(num):
+        if num <= 3:
+            return num
+        elif num == 4:
+            return a
+        elif num == 5:
+            return b
+        elif num == 6:
+            return c
+
+    o = []
+    ip = 0
+    while ip < len(prog):
+        instr = prog[ip]
+        operand = prog[ip + 1]
+        if instr == 0:
+            a = a // (2 ** combo(operand))
+        elif instr == 1:
+            b = b ^ operand
+        elif instr == 2:
+            b = combo(operand) % 8
+        elif instr == 3:
+            if a != 0:
+                ip = operand - 2
+        elif instr == 4:
+            b = b ^ c
+        elif instr == 5:
+            o.append(combo(operand) % 8)
+        elif instr == 6:
+            b = a // (2 ** combo(operand))
+        elif instr == 7:
+            c = a // (2 ** combo(operand))
+        ip += 2
+    return o
+
+print("Output:", *run(prog, a, b, c), sep=",")
+
+# Recursive function
+def rec(n, a):
+    if n == -1:
+        return a
+    a <<= 3
+    for x in range(8):
+        if run(prog, a + x, 0, 0) == prog[n:]:
+            s = rec(n - 1, a + x)
+            if s != -1:
+                return s
+    return -1
+
+print("Result:", rec(len(prog) - 1, 0))
